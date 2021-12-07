b564428bfd0a83f4c40d8a69f97effa243e10ecf
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-07 23:08:28 +0700

    set onclick

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { Div1, Div1Child, Div2, Div2Child, NavStyles } from "./NavStyles";
+import { HiArrowCircleDown } from "react-icons/hi";
+import Dropdown from "./Dropdown";
 
 function Nav() {
   let date = new Date().toDateString().slice(0, 7);
   let time = new Date().toLocaleTimeString();
   const [cTime, setCtime] = useState(time);
+  const [click, setClick] = useState(false);
+
   setInterval(() => {
     time = new Date().toLocaleTimeString();
     setCtime(time);
@@ -12,12 +16,17 @@ function Nav() {
   return (
     <NavStyles>
       <Div1>
-        <Div1Child>
+        <div>
           <img /> <span>Lovelyo Yeremia</span>
-        </Div1Child>
+        </div>
         <Div1Child>File</Div1Child>
         <Div1Child>Help</Div1Child>
         <Div1Child>Settings</Div1Child>
+        <HiArrowCircleDown
+          className="icon-arrow"
+          onClick={() => setClick(!click)}
+        />
+        {click && <Dropdown />}
       </Div1>
       <Div2>
         <Div2Child>mokalulovelyo@gmail.com</Div2Child>
